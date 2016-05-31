daf96f454da9793bc991a2fba80793e1f2cdde8b
alvarhc2 <alvarhc2@gmail.com>
2016-05-31 14:03:42 +0200

    -

--- src/Presentacio/VistaAnteriorsImp.java
+++ src/Presentacio/VistaAnteriorsImp.java
@@ -18,10 +18,13 @@ import javax.swing.GroupLayout;
  */
 public class VistaAnteriorsImp {
     private CtrlPresentacio cp;
+    private VistaRelImportant vrelimp;
+    private Vector<Pair<String, SparseVector>> conjresimps;
 
     public VistaAnteriorsImp(CtrlPresentacio ctrl) {
         initComponents();
         this.cp = ctrl;
+        conjresimps = cp.UltimsImp();
         imprimeixResultats();
     }
 
@@ -30,7 +33,6 @@ public class VistaAnteriorsImp {
     }
 
     private void imprimeixResultats() {
-        Vector<Pair<String, SparseVector>> conjresimps = cp.UltimsImp();
         Vector<String> res = new Vector<>();
         for (int i = 0; i < conjresimps.size(); i++) {
             String id = new String();
@@ -40,7 +42,7 @@ public class VistaAnteriorsImp {
                 ++j;
             }
             ++j;
-            String relacio = cp.get_nom_relacio(conjresimps.get(i).getKey().substring(j,conjresimps.get(i).getKey().length()-1));
+            String relacio = cp.get_nom_relacio(conjresimps.get(i).getKey().substring(j));
             String nom = cp.IDToNom(Integer.parseInt(id), cp.TipusEquilvalent(conjresimps.get(i).getKey().charAt(j)));
             res.add("Nom de la entitat cercada: " + nom);
             res.add("Tipus de relació per a la cerca: " + relacio);
@@ -51,7 +53,26 @@ public class VistaAnteriorsImp {
 
     private void enrereActionPerformed(ActionEvent e) {
         frame.setVisible(false);
+        vrelimp = new VistaRelImportant(cp);
+        vrelimp.ferVisible();
+    }
 
+    private void infoActionPerformed(ActionEvent e) {
+        if(ultres.isSelectionEmpty()) {
+            JOptionPane.showMessageDialog(frame, "Per a informació adicional has de seleccionar un resultat", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        else {
+            resdet.setVisible(true);
+            resdet.pack();
+            resdet.setLocationRelativeTo(frame);
+            frame.setFocusable(false);
+            int i = 0;
+            while(Character.isDigit(conjresimps.get(ultres.getLeadSelectionIndex()/3).getKey().charAt(i))) {++i;}
+            ++i;
+            Character t = conjresimps.get(ultres.getLeadSelectionIndex()).getKey().charAt(i);
+            res.setListData(cp.MostraRelImp(conjresimps.get(ultres.getLeadSelectionIndex()/3).getValue(),
+                    cp.TipusEquilvalent(t)));
+        }
     }
 
     private void initComponents() {
@@ -62,9 +83,14 @@ public class VistaAnteriorsImp {
         label1 = new JLabel();
         scrollPane1 = new JScrollPane();
         ultres = new JList();
-        button2 = new JButton();
+        info = new JButton();
         button3 = new JButton();
         button4 = new JButton();
+        resdet = new JDialog();
+        label2 = new JLabel();
+        scrollPane2 = new JScrollPane();
+        res = new JList();
+        button1 = new JButton();
 
         //======== frame ========
         {
@@ -82,8 +108,9 @@ public class VistaAnteriorsImp {
                 scrollPane1.setViewportView(ultres);
             }
 
-            //---- button2 ----
-            button2.setText("Info");
+            //---- info ----
+            info.setText("Info");
+            info.addActionListener(e -> infoActionPerformed(e));
 
             //---- button3 ----
             button3.setText("Creua Resultats");
@@ -107,7 +134,7 @@ public class VistaAnteriorsImp {
                         .addGap(34, 34, 34))
                     .addGroup(GroupLayout.Alignment.TRAILING, frameContentPaneLayout.createSequentialGroup()
                         .addGap(32, 32, 32)
-                        .addComponent(button2)
+                        .addComponent(info)
                         .addGap(61, 61, 61)
                         .addComponent(button3)
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
@@ -125,13 +152,55 @@ public class VistaAnteriorsImp {
                         .addComponent(scrollPane1, GroupLayout.PREFERRED_SIZE, 168, GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addGroup(frameContentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                            .addComponent(button2)
+                            .addComponent(info)
                             .addComponent(button3)
                             .addComponent(button4))
                         .addContainerGap(15, Short.MAX_VALUE))
             );
             frame.pack();
             frame.setLocationRelativeTo(frame.getOwner());
+        }
+
+        //======== resdet ========
+        {
+            Container resdetContentPane = resdet.getContentPane();
+
+            //---- label2 ----
+            label2.setText("Resultat Important Detallat");
+
+            //======== scrollPane2 ========
+            {
+                scrollPane2.setViewportView(res);
+            }
+
+            //---- button1 ----
+            button1.setText("OK");
+
+            GroupLayout resdetContentPaneLayout = new GroupLayout(resdetContentPane);
+            resdetContentPane.setLayout(resdetContentPaneLayout);
+            resdetContentPaneLayout.setHorizontalGroup(
+                resdetContentPaneLayout.createParallelGroup()
+                    .addGroup(resdetContentPaneLayout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addGroup(resdetContentPaneLayout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                            .addComponent(label2)
+                            .addComponent(scrollPane2, GroupLayout.PREFERRED_SIZE, 333, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(button1))
+                        .addContainerGap(28, Short.MAX_VALUE))
+            );
+            resdetContentPaneLayout.setVerticalGroup(
+                resdetContentPaneLayout.createParallelGroup()
+                    .addGroup(resdetContentPaneLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(label2)
+                        .addGap(18, 18, 18)
+                        .addComponent(scrollPane2, GroupLayout.PREFERRED_SIZE, 187, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(button1)
+                        .addContainerGap(12, Short.MAX_VALUE))
+            );
+            resdet.pack();
+            resdet.setLocationRelativeTo(resdet.getOwner());
         }
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
@@ -143,8 +212,13 @@ public class VistaAnteriorsImp {
     private JLabel label1;
     private JScrollPane scrollPane1;
     private JList ultres;
-    private JButton button2;
+    private JButton info;
     private JButton button3;
     private JButton button4;
+    private JDialog resdet;
+    private JLabel label2;
+    private JScrollPane scrollPane2;
+    private JList res;
+    private JButton button1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
